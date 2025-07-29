a76a61d9488516796553176be714e61a58ca2e3b
kevinsultana <kevinpdg.ks@gmail.com>
2025-07-29 21:10:46 +0700

    feat: added firebase function to login, oauth google and register

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,11 +1,70 @@
 import React, { useState } from "react";
+import Swal from "sweetalert2";
+import { auth, googleProvider } from "../../firebase/firebase";
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import { useNavigate } from "react-router";
+import { FaGoogle } from "react-icons/fa";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
   const handleLogin = (e) => {
     e.preventDefault();
+    setLoading(true);
+    if (!email) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Email is required",
+      });
+      return;
+    }
+    if (!password) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Password is required",
+      });
+      return;
+    }
+    try {
+      const response = signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    }
   };
+
+  const handleRegister = () => {
+    navigate("/auth/register", { replace: true });
+  };
+
+  const handleGoogleSignIn = async () => {
+    try {
+      const oauthLogin = await signInWithPopup(auth, googleProvider);
+      const credential = GoogleAuthProvider.credentialFromResult(oauthLogin);
+      const token = credential.accessToken;
+      console.log(credential);
+      // navigate("/", { replace: true });
+    } catch (error) {
+      console.log(error);
+      Swal.fire("Error Signing in with Google");
+    }
+  };
+
   return (
     <div className="min-h-screen">
       <h1>Login</h1>
@@ -24,6 +83,14 @@ export default function Login() {
         />
         <button onClick={handleLogin}>Login</button>
       </form>
+      <button onClick={handleRegister}>Register</button>
+      <button
+        onClick={handleGoogleSignIn}
+        className="w-3/4 flex items-center justify-center gap-4 text-white bg-red-600 rounded-xl hover:bg-red-700 p-2 my-2 transition-all duration-300"
+      >
+        <FaGoogle />
+        <p>Login with Google</p>
+      </button>
     </div>
   );
 }
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,12 +1,51 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
+import { auth } from "../../firebase/firebase";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [userName, setUserName] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleRegister = (e) => {
     e.preventDefault();
+    setLoading(true);
+    if (!email) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Email is required",
+      });
+      return;
+    }
+    if (!password) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Password is required",
+      });
+      return;
+    }
+    try {
+      const response = createUserWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    }
+  };
+
+  const handleToLogin = () => {
+    navigate("/auth/login", { replace: true });
   };
   return (
     <div className="min-h-screen">
@@ -30,8 +69,9 @@ export default function Register() {
           placeholder="input password here"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button onClick={handleRegister}>Login</button>
+        <button onClick={handleRegister}>Register</button>
       </form>
+      <button onClick={handleToLogin}>Login</button>
     </div>
   );
 }
